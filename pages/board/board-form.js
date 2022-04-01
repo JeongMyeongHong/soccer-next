228655c248ab4fadb534b80975045e206d45fda5
JeongMyeongHong <hkdhjm@gmail.com>
2022-04-01 18:00:38 +0900

    2022-04-01 create dataset

---
+++ pages/board/board-form.js
@@ -0,0 +1,71 @@
+import style from "./styles/board-form.module.css"
+import React, { useState } from "react";
+
+export default function BoardForm(){
+
+    const [inputs, setInputs] = useState({})
+    const {passengerId, name, teamId, subject} = inputs
+
+    const onChange = (e) => {
+        e.preventDefault()
+        const { name, value } = e.target
+        setInputs({
+            ...inputs,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const request = {passengerId, name, teamId, subject}
+        alert(`데이터 셋 출력 : ${JSON.stringify(request)}`)
+    }
+    return(<>
+        <h1>게시판</h1>
+        <div className={style.container}>
+            <form action="">
+            <div className={style.row}>
+                <div className={style.col25}>
+                <label className={style.label} htmlFor="passengerId">사용자 Id</label>
+                </div>
+                <div className={style.col75}>
+                <input className={style.inputText} type="text" id="passengerId" name="passengerId" onChange={onChange} placeholder="1"/>
+                </div>
+            </div>
+            <div className={style.row}>
+                <div className={style.col25}>
+                <label className={style.label} htmlFor="name">사용자 이름</label>
+                </div>
+                <div className={style.col75}>
+                <input className={style.inputText} type="text" id="name" name="name" onChange={onChange} placeholder="Owen"/>
+                </div>
+            </div>
+            <div className={style.row}>
+                <div className={style.col25}>
+                <label className={style.label} htmlFor="teamId">팀 이름</label>
+                </div>
+                <div className={style.col75}>
+                <select onChange={onChange} id="teamId" name="teamId">
+                    <option value="">응원팀 선택</option>
+                    <option value="K09">서울 FC서울</option>    
+                    <option value="K02">수원 삼성블루윙즈</option>
+                    <option value="K04">인천 유나이티드</option>
+                </select>
+                </div>
+            </div>
+            <div className={style.row}>
+                <div className={style.col25}>
+                <label className={style.label} htmlFor="subject">Subject</label>
+                </div>
+                <div className={style.col75}>
+                <textarea id="subject" name="subject" placeholder="화이팅 FC서울" onChange={onChange} style={{height: 200 + 'px'}}></textarea>
+                </div>
+            </div>
+            <br/>
+            <div className={style.row}>
+                <input className={style.inputSubmit} type="submit" value="Submit" onClick={handleSubmit}/>
+            </div>
+            </form>
+            </div>
+    </>)
+}
--- pages/common/Nav.js
+++ pages/common/Nav.js
@@ -9,8 +9,10 @@ export default function Nav(){
                 <li className={style.li}> <Link href='/admin/dashboard'>dashboard</Link></li>
                 <li className={style.li}> <Link href='/basic/basic-list'>basic</Link></li>
                 <li className={style.li}> <Link href='/board/board-list'>board</Link></li>
+                <li className={style.li}> <Link href='/board/board-form'>BoardForm</Link></li>
                 <li className={style.li}> <Link href='/game/game-list'>gameList</Link></li>
                 <li className={style.li}> <Link href='/game/team-list'>teamList</Link></li>
+                <li className={style.li}> <Link href='/game/team-form'>teamForm</Link></li>
                 <li className={style.li}> <Link href='/todo/todo-list'>todoList</Link></li>
                 <li className={style.li}> <Link href='/user/sign-in'>signin</Link></li>
                 <li className={style.li}> <Link href='/user/sign-up'>signup</Link></li>
